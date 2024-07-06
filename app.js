1135c3889a41ef4152094033989caff34580fbf3
Davood <SacredCode1@gmail.com>
2024-07-06 19:47:02 +0330

    Add middleware by the use method
    Module: 05 Working with Express.js
    Video: 004 Adding Middleware

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const express = require('express');
 // Create a new Express application instance and store it in a constant named 'app'.
 const app = express();
 
+// The use method allows us to add a middleware function
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // The third argument is a function itself and calling it allows the request yo continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    // ...
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
