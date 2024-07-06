93e524da2d008edf793b9f4a10e6963e9d688d55
Davood <SacredCode1@gmail.com>
2024-07-06 18:42:36 +0330

    Install Express.js
    Module: 05 Working with Express.js
    Video: 003 Installing Express.js
    
    Basically creating a new app

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
-const routes = require('./routes');
+// Load the Express library and store it in a constant named 'express'.
+const express = require('express');
 
-const server = http.createServer(routes);
+// Create a new Express application instance and store it in a constant named 'app'.
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
