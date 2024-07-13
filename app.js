675fc318182b33149c0269fe1ff7692a3763f5fb
Davood <SacredCode1@gmail.com>
2024-07-13 18:13:07 +0330

    Filter Paths
    Module: 05 Working with Express.js
    Video: 012 Filtering Paths
    We may use same paths for different methods (GET, POST, etc)

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).send('<h1 style="color: red;">Page not found</h1>')
 });
 
 
