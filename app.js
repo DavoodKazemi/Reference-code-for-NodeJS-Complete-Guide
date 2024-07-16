39c52e59f2a16d42d8cb2a48bc11424d03396d0c
Davood <SacredCode1@gmail.com>
2024-07-16 16:38:11 +0330

    Serve static files
    Module: 05 Working with Express.js
    Video: 019 Serving Files Statically
    
    To serve static files (access them through file system, not router or any middleware), we use static() method of the express object. We should pass in a path to this method, a folder  we want to use for static files.
    
    Basically, we grant read access to a folder content (CSS, JS, images, etc).

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 // Import body-parser package which was installed
 const bodyParser = require('body-parser');
 
+
 // Create a new Express application instance and store it in a constant named 'app'.
 const app = express();
 
@@ -11,6 +12,9 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Make the public folder accessible to be able to use the files inside it statically
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
