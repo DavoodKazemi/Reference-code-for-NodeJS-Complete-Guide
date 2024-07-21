d9fa0c6cd84ecad3a11be7fd9bbbdd579769c676
Davood <SacredCode1@gmail.com>
2024-07-22 00:37:04 +0330

    Add Layout to pug
    Indenting is very important.
    
    No closing tags.
    
    We can add any attribute to HTML tags inside the parentheses. But we can add class and id in other ways too. element.class1.class2(attr='val')#id
    
    We can pass arguments as dynamic content from render methods into pug templates like this: #{pageTitle}
    
    We use () to add JS code inside pug files.
    
    We can use layouts to add mutual sections like header. We use block to add unique content from each template that uses that layout. Something like get_template_part() function.

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
@@ -9,16 +9,16 @@ const products = [];
 
 // Replace the app with router
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.render('add-product', {pageTitle: 'Add Product'});
+router.get("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.render("add-product", { pageTitle: "Add Product", path: "/admin/add-product" });
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    products.push({title: req.body.title});
-    res.redirect('/');
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
 exports.routes = router;
