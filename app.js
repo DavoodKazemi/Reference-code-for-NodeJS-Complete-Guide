ce4c5e8ed1253959b51f1468ca2bfe4b621de3d3
Davood <SacredCode1@gmail.com>
2024-07-22 20:56:36 +0330

    Use Handlebars as templating engine
    Although we installed the handlebars package, unlike Pug, we need to manually import it using require. The require function is used to load the 'express-handlebars' module so we can use it in our application.
    
    Then we also should register Handlebars as the template engine for .hbs files (the extension is up to you).
    
    We also should change the 'view engine' property to 'hbs' of course.
    
    To fix errors we should use change expressHbs() to expressHbs.engine() and also we should pass layout: false if we don't use layout.

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-const path = require('path');
+const path = require("path");
 // Load the Express library and store it in a constant named 'express'.
-const express = require('express');
+const express = require("express");
 // Import body-parser package which was installed
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
+// Unlike Pug, handlebars need to be imported.
+const expressHbs = require("express-handlebars");
 
 // Create a new Express application instance and store it in a constant named 'app'.
 const app = express();
 
-// Set the templating engine to Pug using the 'view engine' property
-app.set('view engine', 'pug');
+// Registering Handlebars as the template engine for .hbs files.
+// First argument (extension) is up to you.
+// app.engine function is an Express function used to register a templating engine.
+// expressHbs.engine function initializes Handlebars, making it compatible with Express and allowing us to use Handlebars templates in our app.
+app.engine("hbs", expressHbs.engine());
+// Set the templating engine to Handlebars using the 'view engine' property
+app.set("view engine", "hbs");
 // Set the location of the views directory (default is 'views')
-app.set('views', 'views');
+app.set("views", "views");
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Make the public folder accessible to be able to use the files inside it statically
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found'});
+  res.status(404).render("404", { pageTitle: "Page not found", layout: false });
 });
 
 app.listen(3000);
