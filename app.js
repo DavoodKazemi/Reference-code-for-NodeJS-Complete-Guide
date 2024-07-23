c8ac2f86ce51fb8fd92008bf2a8588228a39b89b
Davood <SacredCode1@gmail.com>
2024-07-23 15:49:37 +0330

    Add Layouts to EJS views using Partials
    In EJS we can't use Layout like PUG or HBS. But we use Template Partials as an alternative.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found", layout: false });
+  res.status(404).render("404", { pageTitle: "Page not found", layout: false, path: '' });
 });
 
 app.listen(3000);
