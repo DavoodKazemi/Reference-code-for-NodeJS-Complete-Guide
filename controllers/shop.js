305a91894c1b69947ded0c47b53abffe8bd8a4d7
Davood <SacredCode1@gmail.com>
2024-08-13 19:34:09 +0330

    Add a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product");
-
+const Cart = require("../models/cart");
 
 
 // Controller function for displaying products lists
@@ -44,7 +44,9 @@ exports.getCart = (req, res, next) => {
 }
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log("productId", prodId);
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect("/cart");
 }
 
