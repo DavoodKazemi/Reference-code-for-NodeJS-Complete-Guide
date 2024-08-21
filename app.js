78c2cb4200fc676ee11b1bcfcb8b68b118620a79
Davood <SacredCode1@gmail.com>
2024-08-21 19:52:51 +0330

    Add Order and Order Item model

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 // Create a new Express application instance and store it in a constant named 'app'.
 const app = express();
@@ -48,6 +50,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through:OrderItem});
 
 sequelize
   // .sync( { force: true } )
